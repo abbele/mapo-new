4e285968bc4ea119a804b6e85a6ddacba85fe002
Michele Abbrescia <michele.abbrescia@lotrek.it>
2026-04-22 17:04:29 +0200

    feat(example): initialize Nuxt 4 app in apps/example

---
+++ apps/example/app/app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useMapo } from "mapomodule";
+
+const mapo = useMapo();
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
