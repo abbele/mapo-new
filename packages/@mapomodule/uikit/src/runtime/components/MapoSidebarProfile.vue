c0e788c5417ccdf3cde92bd877078e91f2067bde
Michele Abbrescia <michele.abbrescia@lotrek.it>
2026-05-05 14:29:57 +0200

    refactor(form): code structure for improved readability and maintainability in field and uikit

--- packages/@mapomodule/uikit/src/runtime/components/MapoSidebarProfile.vue
+++ packages/@mapomodule/uikit/src/runtime/components/MapoSidebarProfile.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useAuthStore } from "@mapomodule/store/runtime/stores/auth";
 import { useMapoAuth } from "@mapomodule/core/runtime/auth/useMapoAuth";
+import { useSidebarStore } from "@mapomodule/store/runtime/stores/sidebar";
 
 withDefaults(
   defineProps<{
@@ -11,6 +12,7 @@ withDefaults(
 );
 
 const auth = useAuthStore();
+const sidebar = useSidebarStore();
 const { logout } = useMapoAuth();
 </script>
 
